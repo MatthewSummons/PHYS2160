6f67ce774740ff709e999f159b291900f8562dde
Matthew <46129554+MatthewSummons@users.noreply.github.com>
2022-04-17 23:52:47 +0800

    Attempt & Solve The Project Problem Q2

--- Project/Non-Linear_Pendulum.py
+++ Project/Non-Linear_Pendulum.py
@@ -1,19 +1,22 @@
-##
+### Non-Linear_Pendulum.py ###
+### Python Program to Numerically Solve Simple Non-Linear Pendulums ###
+### Created by Shaheer Ziya, last Modified on Apr 17, 2022 ###
 
-##
-
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.integrate as sp
-from scipy.misc import derivative
+from cmath import sqrt
+import matplotlib.pyplot as plt     # To plot graphs
+import numpy as np                  # For mathematical functions
+import scipy.integrate as sp        # For numerical integration
+from scipy.misc import derivative   # For numerical differentiation
 
 # g is the gravitaional acceleartion, l is the length of the pendulum
 g, l = 9.81, 1.0
 
-# Initial angle of the pendulum (radians)
-theta_0 = 55 * np.pi /180
+# Initial angle and angular velocity of the pendulum (radians, radians/second)
+theta_0, omega_0 = 165 * np.pi /180, 0
+angleStr, wStr = "165^{\circ}", "0 \ rads^{-1}"
 
-STEPS = int(1e5)
+# Number of steps for the numerical solution
+STEPS = 100000
 
 
 def dr_dt(r, t):
@@ -23,17 +26,20 @@ def dr_dt(r, t):
   return np.array([omega, (-g/l) * np.sin(theta)])
 
 
-def theta_a(t, theta_m, delta):
+def theta_a(t, theta_0, theta_prime_0):
   '''The function for the analytic solution of a linear pendulum under gravity (g) with length (l)'''
+  delta = np.arctan2(-np.sqrt(l/g) * theta_prime_0, theta_0)
+  theta_m = theta_0 / np.cos(delta)
   return theta_m * np.cos(np.sqrt(g/l)* t + delta)
 
 
 def period_function(x, theta_m):
+  '''The integrand for the period of simple non-linear pendulums'''
   return 1 / np.sqrt((np.cos(x) - np.cos(theta_m)))
 
 
 def find_period(theta_m) -> float:
-  
+  '''The function to find the period of the simple non-linear pendulum by numerical integration'''
   Period = 4 * np.sqrt(l / (2*g)) * sp.quad(period_function, 0, theta_m, args=(theta_m))[0]
   return Period
   
@@ -46,7 +52,7 @@ def main():
   t = np.linspace(start, end, STEPS)
   
   # The initial conditions for the ODE (r = <theta, omega> )
-  r0 = np.array([theta_0, 0])  # Note that (-2π ≤ theta ≤ 2π) !!!
+  r0 = np.array([theta_0, omega_0])  # Note that (-2π ≤ theta ≤ 2π) !!!
   
   # Solve the ODE numerically using scipy.integrate.odeint
   sol = sp.odeint(dr_dt,r0, t)
@@ -60,13 +66,17 @@ def main():
   fig1, axs1 = plt.subplots()
 
   axs1.plot(t, theta_of_t, label="Numerical Non-Linear  $\\theta(t)$")
-  axs1.plot(t, theta_a(t, r0[0], 0), label="Analytic Linear Solution $\\theta_a(t)$")
+  axs1.plot(t, theta_a(t, r0[0], r0[1]), label="Analytic Linear Solution $\\theta_a(t)$")
   
   axs1.set_xlabel("Time ($t$)")
   axs1.set_ylabel("Angular Displacement ($\\theta$)")
   
   axs1.legend(loc='best')
-  axs1.set_title(f"Solutions for Linear & Non-linear Pendulums \n $\\theta(0) = {r0[0]}$, $\\omega(0) = 0$")
+  axs1.set_title(
+      f"Solutions for Linear & Non-linear Pendulums \n \
+        $\\theta(0) = {angleStr}$, $\\omega(0) = {wStr}$")
+  
+  plt.savefig(f'{angleStr}+{wStr}--1.1.png', dpi=300)
 
 
   # Part (b)
@@ -76,19 +86,20 @@ def main():
   
   axs2.plot(theta_of_t, omega_of_t, label="Numerical Non-Linear  $\\omega(t)$")
   
-  analytic, dtheta_dt = theta_a(t, r0[0], 0), derivative(theta_a, t, dx=1e-6, args=(r0[0], 0))
+  analytic, dtheta_dt = theta_a(t, r0[0], r0[1]), derivative(theta_a, t, dx=1e-6, args=(r0[0], r0[1]))
   axs2.plot(analytic, dtheta_dt, label="Analytic Linear Solution $\omega_a(t)$")
   
   axs2.set_xlabel("Angular Displacement ($\\theta$)")
   axs2.set_ylabel("Angular Velocity ($\\omega$)")
   axs2.legend(loc='best', frameon=False)
-  axs2.set_title(f"Phase Diagram for Linear & Non-Linear Pendulums\n $\\theta(0) = {r0[0]}$, $\\omega(0) = 0$")
+  axs2.set_title(f"Phase Diagram for Linear & Non-Linear Pendulums\n\
+     $\\theta(0) = {angleStr}$, $\\omega(0) = {wStr}$")
+  
+  plt.savefig(f'{angleStr}+{wStr}--1.2.png', dpi=300)
 
 
   # Part (c)
-  # Ration of True Period to Small Angle Period
-
-
+  # Ratio of True Period to Small Angle Period
   Periods_Array = np.zeros(90)
   for theta_m in range(1, 90+1):
     T = find_period(theta_m * np.pi/180)
@@ -113,6 +124,8 @@ def main():
   axs3.legend(loc='best', frameon=False)
   axs3.set_title("True Period to Small Angle Period Ratios \n for Different Displacement Angles $(T:T_s)$")
 
+  # plt.savefig(f'{angleStr}--1.3.png', dpi=300)
+ 
   plt.show()
 
 
